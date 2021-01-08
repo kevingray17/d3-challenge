c15640eaa4c61b57488bac57d0127f44ba6b2bc8
KG17 <kevingray17@gmail.com>
2021-01-08 10:00:57 -0500

    initial project mapping

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+// read in data from data csv
+
+
+//create scatterplot between two data variables
+
+
+// include state abbreviations in the circles 
+
+
+// create and situate axes and labels to the bottom left of the charset
+
+
+
