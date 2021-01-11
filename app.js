87692218bd464c733067d4fd9c78b49ff6a26f57
KG17 <kevingray17@gmail.com>
2021-01-11 10:08:07 -0500

    x-axis

--- app.js
+++ app.js
@@ -30,9 +30,20 @@ d3.csv("assets/data/data.csv").then(function(data) {
   data.forEach(d => {
     d.obesity = +d.obesity;
     d.smokes = +d.smokes;
-  })
+  });
 
-  
+// create x axis 
+
+var x = d3.scaleLinear()
+  .domain(d3.extent(data.map(d => d.obesity)))
+  .range([0,width]);
+
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(x));
+
+
+
 
 
 
